d3fa64f0d3ca653707ec5f19091ebb614845a773
silverviles <sudarakatharindu2001@gmail.com>
2024-10-14 17:14:20 +0530

    Patient controller invalid return type fix

--- backend/src/main/java/com/csse/mycare/admin/controller/PharmacyController.java
+++ backend/src/main/java/com/csse/mycare/admin/controller/PharmacyController.java
@@ -4,6 +4,8 @@ import com.csse.mycare.admin.dto.PharmacyRegistrationRequest;
 import com.csse.mycare.common.BaseController;
 import com.csse.mycare.common.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin/pharmacy")
 public class PharmacyController extends BaseController {
     @PostMapping("/register-pharmacy")
-    public BaseResponse<Boolean> registerPharmacy(PharmacyRegistrationRequest request) {
+    public ResponseEntity<BaseResponse<Boolean>> registerPharmacy(PharmacyRegistrationRequest request) {
         try {
             log.info("Registering pharmacy: {}", request.getEmail());
             Boolean result = masterService.savePharmacy(request);
             log.info("Pharmacy registered successfully: {}", request.getEmail());
-            return new BaseResponse<>(result);
+            return new ResponseEntity<>(new BaseResponse<>(true), HttpStatus.OK);
         } catch (Exception e) {
             log.error("Error registering pharmacy: {}", request.getEmail(), e);
-            return new BaseResponse<>(false);
+            return new ResponseEntity<>(new BaseResponse<>(false), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
