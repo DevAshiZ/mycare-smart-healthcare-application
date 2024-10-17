714525baafc3256618d85399a1b1cadc86a3b4ea
Ashan Tharindu <153891101+DevAshiZ@users.noreply.github.com>
2024-10-17 21:58:20 +0530

    created get all doctors section in appointment view

--- web-client/src/pages/dashboard.jsx
+++ web-client/src/pages/dashboard.jsx
@@ -21,74 +21,77 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
 import {DayPicker} from "react-day-picker";
-import React from "react";
+import React, {useEffect} from "react";
+import {getAllDoctors} from "../services/doctorService.js";
 
-const DOCTORS = [
-    {
-        name: "Dr. John Doe",
-        specialty: "Cardiologist",
-        rating: 4.5,
-        reviews: 143,
-        description:
-            "A highly experienced cardiologist dedicated to providing expert care in diagnosing and treating heart-related conditions.",
-        image:
-            "https://img.freepik.com/free-photo/doctor-with-his-arms-crossed-white-background_1368-5790.jpg?uid=R103831228&ga=GA1.1.836062334.1725166554&semt=ais_hybrid",
-    },
-    {
-        name: "Dr. Jane Smith",
-        specialty: "Neurologist",
-        rating: 4.8,
-        reviews: 200,
-        description:
-            "Specialized in treating disorders of the nervous system, providing expert care for brain and spinal issues.",
-        image:
-            "https://img.freepik.com/free-photo/female-doctor-holding-medical-chart_23-2148966892.jpg",
-    },
-    {
-        name: "Dr. Alice Brown",
-        specialty: "Pediatrician",
-        rating: 4.6,
-        reviews: 180,
-        description:
-            "Dedicated to providing compassionate care to children, ensuring their health and well-being.",
-        image:
-            "https://img.freepik.com/free-photo/female-doctor-with-crossed-arms-smiling_23-2149074757.jpg",
-    },
-    {
-        name: "Dr. Michael Green",
-        specialty: "Orthopedic Surgeon",
-        rating: 4.7,
-        reviews: 250,
-        description:
-            "Expert in musculoskeletal conditions, providing advanced surgical and non-surgical treatments.",
-        image:
-            "https://img.freepik.com/free-photo/portrait-young-confident-doctor-wearing-glasses-holding-folder_171337-5146.jpg",
-    },
-    {
-        name: "Dr. Emily White",
-        specialty: "Dermatologist",
-        rating: 4.9,
-        reviews: 300,
-        description:
-            "Specializes in diagnosing and treating skin-related issues, offering comprehensive skincare solutions.",
-        image:
-            "https://img.freepik.com/free-photo/portrait-confident-doctor_1139-490.jpg",
-    },
-];
+// const DOCTORS = [
+//     {
+//         name: "Dr. John Doe",
+//         specialty: "Cardiologist",
+//         rating: 4.5,
+//         reviews: 143,
+//         description:
+//             "A highly experienced cardiologist dedicated to providing expert care in diagnosing and treating heart-related conditions.",
+//         image:
+//             "https://img.freepik.com/free-photo/doctor-with-his-arms-crossed-white-background_1368-5790.jpg?uid=R103831228&ga=GA1.1.836062334.1725166554&semt=ais_hybrid",
+//     },
+//     {
+//         name: "Dr. Jane Smith",
+//         specialty: "Neurologist",
+//         rating: 4.8,
+//         reviews: 200,
+//         description:
+//             "Specialized in treating disorders of the nervous system, providing expert care for brain and spinal issues.",
+//         image:
+//             "https://img.freepik.com/free-photo/female-doctor-holding-medical-chart_23-2148966892.jpg",
+//     },
+//     {
+//         name: "Dr. Alice Brown",
+//         specialty: "Pediatrician",
+//         rating: 4.6,
+//         reviews: 180,
+//         description:
+//             "Dedicated to providing compassionate care to children, ensuring their health and well-being.",
+//         image:
+//             "https://img.freepik.com/free-photo/female-doctor-with-crossed-arms-smiling_23-2149074757.jpg",
+//     },
+//     {
+//         name: "Dr. Michael Green",
+//         specialty: "Orthopedic Surgeon",
+//         rating: 4.7,
+//         reviews: 250,
+//         description:
+//             "Expert in musculoskeletal conditions, providing advanced surgical and non-surgical treatments.",
+//         image:
+//             "https://img.freepik.com/free-photo/portrait-young-confident-doctor-wearing-glasses-holding-folder_171337-5146.jpg",
+//     },
+//     {
+//         name: "Dr. Emily White",
+//         specialty: "Dermatologist",
+//         rating: 4.9,
+//         reviews: 300,
+//         description:
+//             "Specializes in diagnosing and treating skin-related issues, offering comprehensive skincare solutions.",
+//         image:
+//             "https://img.freepik.com/free-photo/portrait-confident-doctor_1139-490.jpg",
+//     },
+// ];
 
 export const PatientDashboard = () => {
+    const [selectedDoctor, setSelectedDoctor] = React.useState(null);
+
     return (
         <div>
             <DashboardHeader/>
             <div className="flex gap-4 bg-gray-100 px-4 py-4">
                 <div className="w-3/12">
                     {/*Doctor List*/}
-                    <DoctorListSection/>
+                    <DoctorListSection setSelectedDoctor={setSelectedDoctor}/>
 
                 </div>
                 <div className="w-6/12">
                     {/*Doctor Details*/}
-                    <DoctorDetailsSection/>
+                    <DoctorDetailsSection selectedDoctor={selectedDoctor}/>
 
                 </div>
                 <div className="w-3/12">
@@ -100,11 +103,22 @@ export const PatientDashboard = () => {
         </div>
     )
 }
-function DoctorListSection() {
+function DoctorListSection({setSelectedDoctor}) {
+
+    const [doctors, setDoctors] = React.useState([]);
+
+    useEffect(() => {
+        const fetchDoctors = async () => {
+            const response = await getAllDoctors();
+            setDoctors(response);
+        }
+        fetchDoctors();
+    }, []);
+
     return (
         <div className="h-screen overflow-auto p-2">
-            {DOCTORS.map((doctor, index) => (
-                    <DoctorCard key={index} doctor={doctor} index={index}/>
+            {doctors.map((doctor, index) => (
+                    <DoctorCard key={index} doctor={doctor} index={index} setSelectedDoctor={setSelectedDoctor}/>
                 )
             )}
         </div>
@@ -112,7 +126,7 @@ function DoctorListSection() {
 }
 
 {/* Helper functions */}
-function DoctorDetailsSection() {
+function DoctorDetailsSection({selectedDoctor}) {
     return (
         <div>
             <Card className="w-full  flex-row mb-2">
@@ -131,10 +145,10 @@ function DoctorDetailsSection() {
                     <div className="mb-3 flex items-center justify-between">
                         <div>
                             <Typography variant="h5" color="blue-gray" className="font-semibold font-abril">
-                                Dr. John Doe
+                                {selectedDoctor?.firstName} {selectedDoctor?.lastName}
                             </Typography>
                             <Typography color="gray" className="text-xs font-abril">
-                                Cardiologist
+                                {selectedDoctor?.specialization}
                             </Typography>
                         </div>
                         <Typography
@@ -153,14 +167,14 @@ function DoctorDetailsSection() {
                             <IconButton className="rounded-full" variant="outlined">
                                 <FontAwesomeIcon icon={faUser} className="w-4 h-4"/>
                             </IconButton>
-                            <Typography className="text-sm mt-2 font-bold text-gray-800">116+</Typography>
+                            <Typography className="text-sm mt-2 font-bold text-gray-800">{Math.floor(Math.random() * 200) + 1}+</Typography>
                             <Typography className="text-xs   text-gray-800">Followers</Typography>
                         </div>
                         <div className="flex flex-col items-center">
                             <IconButton className="rounded-full" variant="outlined">
                                 <FontAwesomeIcon icon={faNotesMedical} className="w-4 h-4"/>
                             </IconButton>
-                            <Typography className="text-sm mt-2 font-bold text-gray-800">3+</Typography>
+                            <Typography className="text-sm mt-2 font-bold text-gray-800">{Math.floor(Math.random() * 10) + 1}+</Typography>
                             <Typography className="text-xs   text-gray-800">Years</Typography>
                         </div>
                         <div className="flex flex-col items-center">
@@ -174,7 +188,7 @@ function DoctorDetailsSection() {
                             <IconButton className="rounded-full" variant="outlined">
                                 <FontAwesomeIcon icon={faMessage} className="w-4 h-4"/>
                             </IconButton>
-                            <Typography className="text-sm mt-2 font-bold text-gray-800">96+</Typography>
+                            <Typography className="text-sm mt-2 font-bold text-gray-800">{Math.floor(Math.random() * 60) + 1}k+</Typography>
                             <Typography className="text-xs   text-gray-800">Appointments</Typography>
                         </div>
                     </div>
@@ -316,7 +330,8 @@ function DashboardHeader() {
     );
 }
 
-function DoctorCard({doctor, index}) {
+function DoctorCard({doctor, index , setSelectedDoctor}) {
+
     return (
         <Card key={index} className="h-[10rem] flex-row mb-2">
             <CardHeader
@@ -325,28 +340,31 @@ function DoctorCard({doctor, index}) {
                 className="m-0 w-2/5 shrink-0 rounded-r-none"
             >
                 <img
-                    src={doctor.image}
-                    alt={`${doctor.name}-image`}
+                    src={'https://img.freepik.com/free-photo/doctor-with-his-arms-crossed-white-background_1368-5790.jpg?uid=R103831228&ga=GA1.1.836062334.1725166554&semt=ais_hybrid'}
+                    alt={`${doctor.firstName}-image`}
                     className="h-full w-full object-cover"
                 />
             </CardHeader>
 
             <div className="mt-2 ml-4 pr-2">
                 <Typography variant="h5" color="blue-gray" className="font-semibold ">
-                    {doctor.name}
+                    {doctor.firstName} {doctor.lastName}
                 </Typography>
-                <Typography className="text-xs">{doctor.specialty}</Typography>
+                <Typography className="text-xs">{doctor.specialization}</Typography>
                 <div className="flex items-center gap-2 mt-2 ">
                     <FontAwesomeIcon icon={faStar} className="text-yellow-600 " />
                     <Typography className="text-xs font-semibold text-gray-800">
-                        {doctor.rating} ({doctor.reviews} reviews)
+                        {(Math.random() * 4 + 1).toFixed(1)} ({Math.floor(Math.random() * 5) + 10} reviews)
                     </Typography>
                 </div>
-                <div>
-                    <Typography className="text-xs text-justify mt-2">
-                        {doctor.description}
-                    </Typography>
+                <div className={'mt-5 justify-center flex'}>
+                    <Button className={'bg-green-500 rounded-lg'} size={"sm"} onClick={()=> setSelectedDoctor(doctor)}>Channel Doctor</Button>
                 </div>
+                {/*<div>*/}
+                {/*    <Typography className="text-xs text-justify mt-2">*/}
+                {/*        {doctor.description}*/}
+                {/*    </Typography>*/}
+                {/*</div>*/}
             </div>
         </Card>
     );
@@ -354,12 +372,24 @@ function DoctorCard({doctor, index}) {
 
 DoctorCard.propTypes = {
     doctor: PropTypes.shape({
-        image: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        specialty: PropTypes.string.isRequired,
-        rating: PropTypes.number.isRequired,
-        reviews: PropTypes.number.isRequired,
-        description: PropTypes.string.isRequired,
+        firstName: PropTypes.string.isRequired,
+        lastName: PropTypes.string.isRequired,
+        specialization: PropTypes.string.isRequired,
     }).isRequired,
     index: PropTypes.number.isRequired,
+    setSelectedDoctor: PropTypes.func.isRequired,
 };
+
+DoctorListSection.propTypes = {
+    setSelectedDoctor: PropTypes.func.isRequired,
+};
+
+DoctorDetailsSection.propTypes = {
+    selectedDoctor: PropTypes.shape({
+        firstName: PropTypes.string.isRequired,
+        lastName: PropTypes.string.isRequired,
+        specialization: PropTypes.string.isRequired,
+    }),
+};
+
+
